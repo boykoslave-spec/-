6401e43d2b086056533c9dbd593f591162c8bf92
boykoslave-spec <boykoslave@gmail.com>
2026-07-09 16:42:24 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from aiogram import Bot, Dispatcher
 from aiogram.filters import Command
-from aiogram.types import Message
+from aiogram.types import Message, ReplyKeyboardMarkup, KeyboardButton
 import asyncio
 import os
 
@@ -11,7 +11,19 @@ if not TOKEN:
 
 bot = Bot(TOKEN)
 dp = Dispatcher()
-
+menu = ReplyKeyboardMarkup(
+    keyboard=[
+        [
+            KeyboardButton(text="🪖 Особовий склад"),
+            KeyboardButton(text="📊 Статистика")
+        ],
+        [
+            KeyboardButton(text="🎯 Цілі"),
+            KeyboardButton(text="⚙️ Налаштування")
+        ]
+    ],
+    resize_keyboard=True
+)
 
 @dp.message(Command("start"))
 async def start(message: Message):
