a44225cfc73be61a44c7ec4caa1ffb8e1793c6de
boykoslave-spec <boykoslave@gmail.com>
2026-07-10 02:48:49 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1695,8 +1695,25 @@ async def text_handler(message: Message):
             (
                 action["title"],
                 action["description"],
-                action["event_date"],
-                message.text,
+                try:
+    date_obj = datetime.strptime(
+        message.text,
+        "%d.%m.%Y"
+    )
+
+    if date_obj.date() < datetime.now().date():
+        await message.answer(
+            "❌ Не можна створити подію в минулому."
+        )
+        return
+
+except:
+    await message.answer(
+        "❌ Невірна дата.\n\nПриклад: 15.07.2026"
+    )
+    return
+
+action["event_date"] = message.text
                 int(time.time())
             )
         )
